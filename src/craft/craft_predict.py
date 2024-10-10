7485203c295003ae3f62980d065ee928259eb9a5
tranvandiepp2105 <tranvandiepp2105@gmail.com>
2024-10-10 05:49:56 +0000

    change detect

--- src/craft/craft_predict.py
+++ src/craft/craft_predict.py
@@ -63,3 +63,60 @@ def predict_craft(net, image_path, text_threshold=0.65, cuda_state=False):
     polys = test_net(net, image, text_threshold=text_threshold, cuda=cuda_state)
     polys = [i.tolist() for i in polys]
     return polys
+
+def test_net_batch(net, images, text_threshold, cuda):
+    # Resize tất cả các ảnh trong batch và lưu tỷ lệ
+    img_resized_list = []
+    ratio_w_list = []
+    ratio_h_list = []
+
+    for image in images:
+        img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, canvas_size, interpolation=cv2.INTER_LINEAR, mag_ratio=mag_ratio)
+        img_resized_list.append(img_resized)
+        ratio_h_list.append(1 / target_ratio)
+        ratio_w_list.append(1 / target_ratio)
+
+    # Preprocessing: chuyển đổi tất cả các ảnh trong batch thành tensor
+    x_list = [torch.from_numpy(imgproc.normalizeMeanVariance(img_resized)).permute(2, 0, 1) for img_resized in img_resized_list]
+    x = torch.stack([Variable(x) for x in x_list])  # Stack thành batch
+
+    if cuda:
+        x = x.cuda()
+
+    # Forward pass cho cả batch
+    with torch.no_grad():
+        y, _ = net(x)  # Không cần dùng 'feature' nên bỏ qua
+
+    # Xử lý từng ảnh trong batch
+    polys_batch = []
+    for i in range(len(images)):
+        # Tạo score và link map cho mỗi ảnh
+        score_text = y[i, :, :, 0].cpu().data.numpy()
+        score_link = y[i, :, :, 1].cpu().data.numpy()
+
+        # Post-processing để lấy bounding boxes và polys
+        boxes, polys = craft_utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+
+        # Điều chỉnh tọa độ theo tỷ lệ resize
+        boxes = craft_utils.adjustResultCoordinates(boxes, ratio_w_list[i], ratio_h_list[i])
+        polys = craft_utils.adjustResultCoordinates(polys, ratio_w_list[i], ratio_h_list[i])
+
+        for k in range(len(polys)):
+            if polys[k] is None:
+                polys[k] = boxes[k]
+
+        polys_batch.append([poly.tolist() for poly in polys])
+
+    return polys_batch
+
+
+
+def predict_craft_batch(net, image_paths, text_threshold=0.65, cuda_state=False):
+    # Load tất cả ảnh
+    images = [imgproc.loadImage(image_path) for image_path in image_paths]
+
+    # Gọi hàm test theo batch
+    polys_list = test_net_batch(net, images, text_threshold=text_threshold, cuda=cuda_state)
+
+    return polys_list
+
